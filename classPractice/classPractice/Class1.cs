83f64f0f78d158f1afa39161efb40c12ea6bd55f
KimJongHoss <haberde97@gmail.com>
2024-07-15 12:26:07 +0900

    add classPractice again

---
+++ classPractice/classPractice/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classPractice
+{
+    public class Square
+    {
+
+        string MyName = "사각형";
+
+        public string GetName()
+        {
+        return MyName; 
+        }
+
+    }
+}
---
+++ classPractice/classPractice/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace classPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //int a;
+
+            Square square = new Square();
+            textBox1.Text = square.GetName();
+
+        }
+    }
+}
