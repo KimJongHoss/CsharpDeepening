eefa18eccfe539f20ff99b8b8ba6b07204eb763b
KimJongHoss <haberde97@gmail.com>
2024-07-15 10:33:44 +0900

    first Practice

---
+++ struct_arrowFunctionPrac/structPractice/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace structPractice
+{
+
+    struct Point
+    {
+        public int x; 
+        public int y;
+        public int z;
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Point p = new Point();
+
+            //p.x = 13;
+            //p.y = 21;
+            //p.z = -4;
+
+            //textBox1.Text = $"point({p.x}, {p.y}, {p.z})";
+
+            //Point[] p = new Point[3];
+
+            //for (int i = 0; i < p.Length; i++)
+            //{
+            //    p[i].x = i;
+            //    p[i].y = i * i;
+            //    p[i].z = -i;
+
+            //    textBox1.Text += $"point({p[i].x}, {p[i].y} ,  {p[i].z})\r\n";
+
+            //}
+
+            Hi();
+            textBox1.Text += Add(4, 2).ToString();
+
+        }
+
+        void Hi() => textBox1.Text += "Hi\r\n";
+
+        int Add(int a, int b) => a + b; //arrow 함수는 마지막 줄이 return된다.
+    }
+}
