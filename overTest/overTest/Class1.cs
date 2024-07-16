f5bf6bf77bb6ec0073d1f37efb9221afa10b03b2
KimJongHoss <haberde97@gmail.com>
2024-07-16 12:04:58 +0900

    오버로드/오버라이딩 예제 추가

---
+++ overTest/overTest/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overTest
+{
+    internal class M0
+    {
+
+        public int GetNumber(int n) => n;
+
+        public int GetNumber(int n1, int n2) => n1+n2;
+
+        public long GetNumber(long n) => n-99;
+
+    }
+
+    public class Shape
+    {
+        public string Say() => "Hi\r\n";
+        public string Go() => "Go\r\n";
+        public virtual string getShape() => "Shape\r\n";
+    }
+
+    public class Square : Shape
+    {
+        public string Say() => "Hello\r\n";
+        public string Go() => "GoGoGo\r\n";
+        public override string getShape() => "Square\r\n";
+    }
+
+}
---
+++ overTest/overTest/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace overTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //M0 m0 = new M0();
+
+            //textBox1.Text = m0.GetNumber(10).ToString()+"\r\n";
+            //textBox1.Text += m0.GetNumber(10, 20).ToString() + "\r\n";
+            //textBox1.Text += m0.GetNumber((long)100).ToString();
+
+            Square square = new Square();
+
+            textBox1.Text = square.Say();
+            textBox1.Text += square.Go();
+            textBox1.Text += square.getShape();
+
+        }
+
+       
+
+    }
+}
