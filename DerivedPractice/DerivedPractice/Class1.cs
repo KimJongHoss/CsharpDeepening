3ccf0fcd0e808dfc52eff2bd5d0b99ca26d3d3fb
KimJongHoss <haberde97@gmail.com>
2024-07-16 09:28:38 +0900

    상속 예제 추가

---
+++ DerivedPractice/DerivedPractice/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DerivedPractice
+{
+    public class Shape
+    {
+        protected string getShape() => "Shape";
+    }
+
+    public class Square : Shape
+    {
+        public Square() => MessageBox.Show(getShape());
+        
+    }
+}
