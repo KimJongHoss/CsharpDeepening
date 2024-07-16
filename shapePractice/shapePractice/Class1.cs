4bbfef95a8c674dd4b7c992277f12694ad94e689
KimJongHoss <haberde97@gmail.com>
2024-07-16 12:41:45 +0900

    overwrite의 중요성

---
+++ shapePractice/shapePractice/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shapePractice
+{
+    public class shape
+    {
+        public string getShape() => "shape\r\n";
+    }
+
+    public class circle : shape
+    {
+        public string getShape() => "circle\r\n";
+    }
+
+    public class triangle : shape 
+    {
+        public string getShape() => "triangle\r\n";
+    }
+
+    public class square : shape
+    {
+        public string getShape() => "square\r\n";
+    }
+}
---
+++ shapePractice/shapePractice/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shapePractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            circle circle = new circle();
+            circle circle1 = new circle();
+            circle circle2 = new circle();
+            triangle triangle = new triangle();
+            triangle triangle1 = new triangle();
+            triangle triangle2 = new triangle();
+            square square = new square();
+            square square1 = new square();
+            square square2 = new square();
+            square square3 = new square();
+
+            textBox1.Text += square.getShape();
+            textBox1.Text += circle.getShape();
+            textBox1.Text += square1.getShape();
+            textBox1.Text += triangle.getShape();
+            textBox1.Text += square2.getShape();
+            textBox1.Text += circle1.getShape();
+            textBox1.Text += triangle1.getShape();
+            textBox1.Text += square3.getShape();
+            textBox1.Text += triangle2.getShape();
+            textBox1.Text += circle2.getShape();
+
+        }
+    }
+}
