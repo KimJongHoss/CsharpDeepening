0086907e1d5cb9b1e7fc1a570fb5c5897d9a5508
KimJongHoss <haberde97@gmail.com>
2024-07-16 11:24:08 +0900

    상속 예제 추가

---
+++ weaponTest/weaponTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace weaponTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //라이플 전투력 출력
+            Rifle rifle = new Rifle();
+            textBox1.Text = "라이플 전투력 : "+ rifle.CombatPoint().ToString()+"\r\n";
+
+            //나이프 전투력 출력
+            Knife knife = new Knife();
+            textBox1.Text += "나이프 전투력 : "+ knife.CombatPoint().ToString();
+
+        }
+
+        
+
+       
+    }
+}
---
+++ weaponTest/weaponTest/weapon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weaponTest
+{
+    public class weapon
+    {
+        int attack;//공격력
+        int attack_speed;//공격속도
+
+        protected weapon()//생성자
+        {
+            attack = 5;
+            attack_speed = 10;
+        }
+
+        protected int[] getInfo()
+        {
+            int[] info = new int[2];
+            info[0] = attack;
+            info[1] = attack_speed;
+            return info;
+        }
+    }
+
+    public class Rifle : weapon
+    {
+        int bullet = 10;
+        
+        public int CombatPoint() => getInfo()[0] * getInfo()[1] * bullet;
+        
+    }
+
+    public class Knife : weapon
+    {
+        int acuity = 5;
+        public int CombatPoint() => getInfo()[0] * getInfo()[1] * acuity;
+
+    }
+
+}
