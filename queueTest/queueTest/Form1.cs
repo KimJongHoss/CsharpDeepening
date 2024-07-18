40a4266675545b6e37c387e6b2dd037c2c48ec40
KimJongHoss <haberde97@gmail.com>
2024-07-18 13:40:14 +0900

    stack 예제 추가, queue 예제 추가

---
+++ queueTest/queueTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace queueTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Queue queue = new Queue();
+
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            try
+            {
+                int queue_size = queue.Count;
+                for (int i = 0; i < queue_size + 1; i++)
+                {
+                    textBox1.Text += $"{queue.Dequeue()}\r\n";
+                }
+            }
+            catch (Exception e)
+            {
+                textBox1.Text += e.Message;
+            }
+
+        }
+    }
+}
