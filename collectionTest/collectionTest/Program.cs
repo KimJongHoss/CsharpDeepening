34b4ca08789c05311b4caaca06325e3355558848
KimJongHoss <haberde97@gmail.com>
2024-07-18 13:19:08 +0900

    스택 예제 및 실습 추가

---
+++ collectionTest/collectionTest/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//int[] arr = { 1, 3, 5, 2, 6, 7 };
+//string[] s_arr = { "10", "32", "1", "8"};
+
+//Array.Sort(arr);
+//Console.WriteLine("Sorted array : " + string.Join(", ", arr));
+
+//Array.Reverse(arr);
+//Console.WriteLine("Revuersed array : " +string.Join(", ", arr));
+
+//Array.Sort(s_arr);
+//Console.WriteLine("Sorted array : " + string.Join(", ", s_arr)); //string은 사전 순
+
+//int[] i_arr = Array.ConvertAll(s_arr, int.Parse);
+//Console.WriteLine("Converted int array" + string.Join(",", i_arr));
+//Array.Sort(i_arr);
+//Console.WriteLine("Sorted Array : " + string.Join(",", i_arr));
+
+//int[] arr2 = { 1, 3, 5, 2, 6, 7, 10 };
+//int[] arr3 = arr2.OrderBy(x => x).ToArray();
+
+//int[] intArr = { 3, 1, 2 };
+//string[] strArr = Array.ConvertAll(intArr, x => x.ToString());
+//Console.WriteLine("strArr: " + string.Join(", ", strArr));
+
+
+
+
+
+//[실습] int[] intArr = { 3, 1, 2 };
+//ConvertAll을 이용하여 2배로 키운 값을 가지는 string [] 를 만들어라!
+int[] intsArr = { 3, 1, 2 };
+string[] strsArr = Array.ConvertAll(intsArr, x => (x*2).ToString());
+Console.WriteLine("strsArr: " + string.Join(", ", strsArr));
---
+++ stackTest/stackTest/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace stackTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Stack stack = new Stack();
+
+            //stack.Push("일");
+            //stack.Push("이");
+            //stack.Push("삼");
+
+            //try
+            //{
+            //    int stack_size = stack.Count;
+            //    for (int i = 0; i < stack_size+1; i++)
+            //    {
+            //        textBox1.Text += $"{stack.Peek()}\r\n";
+            //        textBox1.Text += $"{stack.Pop()}\r\n";
+            //    }
+
+            //}
+            //catch (Exception e)
+            //{
+            //    textBox1.Text += e.Message;
+            //}
+
+            printP(1);
+        
+    }
+        //[보너스 문제] 삼 이 일 출력하는 기능을 재귀함수로 구현
+   
+
+    void printP(int c)
+    {
+        if (c == 4) { return; }
+        printP(c + 1);
+        switch (c)
+        {
+            case 1:
+                textBox1.Text += "일\r\n";
+                break;
+            case 2:
+                textBox1.Text += "이\r\n";
+                break;
+            case 3:
+                textBox1.Text += "삼\r\n";
+                break;
+        }
+    }
+
+}
+}
